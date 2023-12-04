6749316f12a02cefaeab43a17db1ac3cda606be8
unknown <ohid91@gmail.com>
2023-12-04 13:34:47 +0600

    demo for preveiw

---
+++ assets/js/main.js
@@ -0,0 +1,147 @@
+(function ($) {
+    "use strict";
+
+    //========== Preloader Start =========//
+    $(window).on('load', function () {
+        $('.preloader').fadeOut(1000);
+    });
+    //========== Preloader Start =========//
+
+
+
+
+    //========== Lightcase Start =========//
+    $('a[data-rel^=lightcase]').lightcase();
+
+    //========== Preloader End =========//
+
+
+
+    $(document).ready(function () {
+
+        //========== Header  Start =========//
+
+        $("ul>li>.submenu").parent("li").addClass("menu-item-has-children");
+        // drop down menu width overflow problem fix
+        $('ul').parent('li').on('hover', function () {
+            var menu = $(this).find("ul");
+            var menupos = $(menu).offset();
+            if (menupos.left + menu.width() > $(window).width()) {
+                var newpos = -$(menu).width();
+                menu.css({
+                    left: newpos
+                });
+            }
+        });
+
+        $('.menu li a').on('click', function (e) {
+            var element = $(this).parent('li');
+            if (element.hasClass('open')) {
+                element.removeClass('open');
+                element.find('li').removeClass('open');
+                element.find('ul').slideUp(300, "swing");
+            } else {
+                element.addClass('open');
+                element.children('ul').slideDown(300, "swing");
+                element.siblings('li').children('ul').slideUp(300, "swing");
+                element.siblings('li').removeClass('open');
+                element.siblings('li').find('li').removeClass('open');
+                element.siblings('li').find('ul').slideUp(300, "swing");
+            }
+        })
+        $('.ellepsis-bar').on('click', function (e) {
+            var element = $('.header-top');
+            if (element.hasClass('open')) {
+                element.removeClass('open');
+                element.slideUp(300, "swing");
+                $('.overlayTwo').removeClass('active');
+            } else {
+                element.addClass('open');
+                element.slideDown(300, "swing");
+                $('.overlayTwo').addClass('active');
+            }
+        });
+        $('.header-bar').on('click', function () {
+            $(this).toggleClass('active');
+            $('.menu').toggleClass('active');
+        })
+
+        //Header
+        var fixed_top = $("header");
+        $(window).on('scroll', function () {
+            if ($(this).scrollTop() > 300) {
+                fixed_top.addClass("header-fixed fadeInUp");
+            } else {
+                fixed_top.removeClass("header-fixed fadeInUp");
+            }
+        });
+
+        //========== Header End =========//
+
+
+
+
+
+
+        //========== Scroll to top Start =========//
+        $(function () {
+            $(window).on('scroll', function () {
+                if ($(this).scrollTop() > 300) {
+                    $('.scrollToTop').css({
+                        'bottom': '5%',
+                        'opacity': '1',
+                        'transition': 'all .5s ease'
+                    });
+                } else {
+                    $('.scrollToTop').css({
+                        'bottom': '-30%',
+                        'opacity': '0',
+                        'transition': 'all .5s ease'
+                    })
+                }
+            });
+
+            //Click event to scroll to top
+            $('a.scrollToTop').on('click', function () {
+                $('html, body').animate({
+                    scrollTop: 0
+                }, 500);
+                return false;
+            });
+        });
+
+        //========== Scroll to to top end =========//
+
+
+        // wow animation
+        // new WOW().init();
+
+
+
+
+        //====demos slider================
+        var swiper = new Swiper(".demos__slider", {
+            slidesPerView: 1,
+            spaceBetween: 20,
+            grabCursor: true,
+            loop: true,
+            autoplay: {
+                delay: 3000,
+                disableOnInteraction: false,
+            },
+            breakpoints: {
+                576: {
+                    slidesPerView: 2,
+                },
+                1200: {
+                    slidesPerView: 3,
+                },
+
+            },
+        });
+
+
+    });
+
+
+}(jQuery));
